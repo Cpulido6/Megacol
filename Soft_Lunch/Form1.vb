31b4b9a51f381d36ebfa1340b98dc18fef563cd1
Cpulido <cepume91@hotmail.com>
2018-03-03 13:01:00 -0500

    Subo la estructura del proyecto

--- Soft_Lunch/Form1.vb
+++ Soft_Lunch/Form1.vb
@@ -1,3 +1,39 @@
-﻿Public Class Form1
+﻿Imports AxZKFPEngXControl
+Imports ZKFPEngXControl
+Public Class Form1
+    Implements ZKFPEngXControl.IZKFPEngXEvents
+    Private Captura As ZKFPEngXControl.ZKFPEngX
 
+
+    Public Sub OnFeatureInfo(AQuality As Integer) Implements IZKFPEngXEvents.OnFeatureInfo
+
+    End Sub
+
+    Public Sub OnImageReceived(ByRef AImageValid As Boolean) Implements IZKFPEngXEvents.OnImageReceived
+
+    End Sub
+
+    Public Sub OnEnroll(ActionResult As Boolean, ATemplate As Object) Implements IZKFPEngXEvents.OnEnroll
+
+    End Sub
+
+    Public Sub OnCapture(ActionResult As Boolean, ATemplate As Object) Implements IZKFPEngXEvents.OnCapture
+
+    End Sub
+
+    Public Sub OnCaptureToFile(ActionResult As Boolean) Implements IZKFPEngXEvents.OnCaptureToFile
+
+    End Sub
+
+    Public Sub OnEnrollToFile(ActionResult As Boolean) Implements IZKFPEngXEvents.OnEnrollToFile
+
+    End Sub
+
+    Public Sub OnFingerTouching() Implements IZKFPEngXEvents.OnFingerTouching
+
+    End Sub
+
+    Public Sub OnFingerLeaving() Implements IZKFPEngXEvents.OnFingerLeaving
+
+    End Sub
 End Class
